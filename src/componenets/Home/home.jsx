7f6ee0cab9ada06f331f7a4e12a86dfe1615fe84
mdriajul01 <mdriajulislam9026@gmail.com>
2024-08-18 19:15:49 +0600

    edu-p

---
+++ src/componenets/Home/home.jsx
@@ -0,0 +1,13 @@
+// @flow strict
+
+import * as React from 'react';
+
+function home() {
+    return (
+        <div>
+            this is Home
+        </div>
+    );
+};
+
+export default home;
---
+++ src/componenets/navber/navber.jsx
@@ -0,0 +1,18 @@
+// @flow strict
+
+import * as React from 'react';
+import logo from "../../../public/image/LOGO.png"
+function navber() {
+    return (
+        <div>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+            </div>
+            <div></div>
+        </div>
+    );
+};
+
+export default navber;
